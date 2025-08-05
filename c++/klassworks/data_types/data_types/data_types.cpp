66ac97ceff78f5600d7836a10f3b4cf9853ffc1f
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-08-05 11:24:45 +0400

    data_types

---
+++ c++/klassworks/data_types/data_types/data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double degree = 0;
+    const double Pi = 3.14;
+    double radian = 0;
+    cout << "Enter degree :", cin >> degree;
+    radian = degree * (Pi / 180);
+    cout << radian << endl;
+
+
+  
+}
+
