0d87b71848ea7abcce1a7c9fdac5723ebd173c9c
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-08-05 11:07:19 +0400

    date_types

---
+++ c++/homeworks/data_types/data_types/data_types.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{          /*ZADANIYE N1
+	int double float char;*/
+	/*..............................................*/
+	/*int num = ' ';
+	cout << sizeof(num) << endl;
+	cout << "Enter num : ", cin >> num;*/
+	/*..............................................*/
+	/*double num = ' ';
+	cout << sizeof(num) << endl;
+	cout << "Enter num : ", cin >> num;*/
+	/*..............................................*/
+	/*float num = ' ';
+	cout << sizeof(num) << endl;
+	cout << "Enter num : ", cin >> num;*/
+	/*..............................................*/
+	/*char i = ' ';
+	cout << sizeof(i) << endl;
+	cout << "Enter num : ", cin >> i;
+	*/
+	/*..............................................*/
+
+
+			 /* ZADANIYE N2*/
+
+	/*double num1 = 0;
+	double num2 = 0;
+	double num3 = 0;
+	cout << "Enter number1 : ", cin >> num1;
+	cout << "Enter number2 : ", cin >> num2;
+	cout << "Enter number3 : ", cin >> num3;
+	double sum = num1 + num2 + num3;
+	if (sum == 0 )
+	{
+		cout << sum << endl;
+	}
+	else if (sum > 0)
+	{
+			cout << sum << endl;
+	}
+	else
+	{
+		cout << sum << endl;
+	}*/
+	/*...............................................*/
+
+					 /*ZADANIYE N3*/
+
+			  /*NE ZNAYU KUDA VVODIT FORMULU*/
+
+
+
+
+
+
+
+
+		/*int num = 0;
+		char i = ' ';
+		cout << "Enter num : ", cin >> i;
+		num = int(i);
+		num++;
+		cout <<char(num)<< endl;*/
+
+		/*char i = ' ';
+		cout << sizeof(i) << endl;
+		cout << "Enter num : ", cin >> i;
+		i++;
+		cout<<*/
+
+
+		/*char text = ' ';
+		cout << "Enter symbol : ", cin >> text;
+		cout << int(text) << endl;*/
+
+
+
+		/*
+		int num = 0;
+		cout << "Enter num : ", cin >> num ;
+		if (num > 256)
+		{
+			cout << "ERROR" << endl;
+		}
+		else
+		{
+			cout << char(num) << endl;
+		}*/
+
+}
